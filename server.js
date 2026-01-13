2ee9df4187daaf5b6fa7f08d260f7d69c817c164
KaeTheDev <kaethedev@gmail.com>
2026-01-13 15:24:55 -0500

    Created a route to / . async with a try catch. if it connects succesfully you get a success message if not it returns a 500 failure message

--- server.js
+++ server.js
@@ -9,3 +9,13 @@ const PORT = process.env.PORT || 3001;
 const uri = process.env.MONGO_URI;
 
 const client = new MongoClient(uri);
+
+app.get('/', async (req, res) => {
+    try {
+        await client.connect();
+        res.json({ message: "Successfully connected to the database!" });
+    } catch(err) {
+        res.status(500).json({ message: "Failed to connecte to the database." });
+    }
+});
+
